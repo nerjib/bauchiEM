11ed437e266ef248be16cada04c461c1ee7eead3
nerjib <kabirnajib0@gmail.com>
2022-01-13 22:59:03 +0100

    Updates

---
+++ precache-manifest.069cb2b6826d1f7778130f1b20684836.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f5af90dd68e394e7b8dfc99532dd2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87912b9d80197aac9ad5",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "693cfe4590eea4bc8aef",
+    "url": "/static/css/main.e3f071c8.chunk.css"
+  },
+  {
+    "revision": "87912b9d80197aac9ad5",
+    "url": "/static/js/2.f66b5fbc.chunk.js"
+  },
+  {
+    "revision": "3f2ad72d1d7871920b9567ec2e765bb9",
+    "url": "/static/js/2.f66b5fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693cfe4590eea4bc8aef",
+    "url": "/static/js/main.0eff1cf5.chunk.js"
+  },
+  {
+    "revision": "9d1948da6512e239c4cd",
+    "url": "/static/js/runtime-main.f9fa5b6e.js"
+  },
+  {
+    "revision": "636c3af61771f8f62f703413ec253a68",
+    "url": "/static/media/kaduna-map.636c3af6.png"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "02437f3f4165922e6c7e9af6b239a007",
+    "url": "/static/media/ruwasa.02437f3f.jpg"
+  },
+  {
+    "revision": "2b0f3c4d5792684eddbbd1e10638dfb1",
+    "url": "/static/media/ruwasa.2b0f3c4d.png"
+  },
+  {
+    "revision": "59d726b5100eba9e183590ea2385b97a",
+    "url": "/static/media/ruwasa.59d726b5.jpg"
+  },
+  {
+    "revision": "41fd881bc2d54d72e92737e34ef0037d",
+    "url": "/static/media/ruwassa.41fd881b.PNG"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "c93fc9232362e91fcb847db2ee7e7bcd",
+    "url": "/static/media/sidebar-2.c93fc923.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "ed6e184d125128ea2b6b7ac81ccc5e01",
+    "url": "/static/media/ukaidd.ed6e184d.jpg"
+  },
+  {
+    "revision": "d9796521f21d6d4e1b39e1a6ba8848c6",
+    "url": "/static/media/unicef.d9796521.png"
+  }
+]);
