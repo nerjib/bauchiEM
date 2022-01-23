8ce25f014b74ad7f6876a39a5bf4b570da0c0448
nerjib <kabirnajib0@gmail.com>
2022-01-23 15:00:34 +0100

    Updates

---
+++ precache-manifest.8aca937ca3b46aeae5e63cde4e814dfd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a185bb567f545f52cbf8d2d4079fe41",
+    "url": "/bauchiEM/index.html"
+  },
+  {
+    "revision": "8fbe4eb5e8c6b5de9114",
+    "url": "/bauchiEM/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "7f3df4f68a0dfc49f82a",
+    "url": "/bauchiEM/static/css/main.e3f071c8.chunk.css"
+  },
+  {
+    "revision": "8fbe4eb5e8c6b5de9114",
+    "url": "/bauchiEM/static/js/2.b5709db0.chunk.js"
+  },
+  {
+    "revision": "3f2ad72d1d7871920b9567ec2e765bb9",
+    "url": "/bauchiEM/static/js/2.b5709db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f3df4f68a0dfc49f82a",
+    "url": "/bauchiEM/static/js/main.e34ab925.chunk.js"
+  },
+  {
+    "revision": "ef1ba77c7f2ea5e82a0b",
+    "url": "/bauchiEM/static/js/runtime-main.dc8d09ea.js"
+  },
+  {
+    "revision": "636c3af61771f8f62f703413ec253a68",
+    "url": "/bauchiEM/static/media/kaduna-map.636c3af6.png"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/bauchiEM/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "02437f3f4165922e6c7e9af6b239a007",
+    "url": "/bauchiEM/static/media/ruwasa.02437f3f.jpg"
+  },
+  {
+    "revision": "2b0f3c4d5792684eddbbd1e10638dfb1",
+    "url": "/bauchiEM/static/media/ruwasa.2b0f3c4d.png"
+  },
+  {
+    "revision": "59d726b5100eba9e183590ea2385b97a",
+    "url": "/bauchiEM/static/media/ruwasa.59d726b5.jpg"
+  },
+  {
+    "revision": "41fd881bc2d54d72e92737e34ef0037d",
+    "url": "/bauchiEM/static/media/ruwassa.41fd881b.PNG"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/bauchiEM/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "c93fc9232362e91fcb847db2ee7e7bcd",
+    "url": "/bauchiEM/static/media/sidebar-2.c93fc923.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/bauchiEM/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/bauchiEM/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "ed6e184d125128ea2b6b7ac81ccc5e01",
+    "url": "/bauchiEM/static/media/ukaidd.ed6e184d.jpg"
+  },
+  {
+    "revision": "d9796521f21d6d4e1b39e1a6ba8848c6",
+    "url": "/bauchiEM/static/media/unicef.d9796521.png"
+  }
+]);
